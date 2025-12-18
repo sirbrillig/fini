a1c04222d21b6b0983ca955accde173f7920eb0f
Payton Swick <payton@foolord.com>
2025-12-17 23:56:51 -0500

    Move status printing to TaskItemWithStatus newtype

--- src/task_item.rs
+++ src/task_item.rs
@@ -41,21 +41,7 @@ impl fmt::Display for Status {
 
 impl fmt::Display for TaskItem {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self.status {
-            // Archived status doesn't require any extra formatting because it will never be mixed
-            // with other statuses and will never be starred.
-            Status::Archived => {
-                write!(f, "{}", self.title)?;
-            }
-            _ => {
-                if self.star.is_some_and(|v| v) {
-                    write!(f, "{} ", "★".yellow())?;
-                } else {
-                    write!(f, "  ")?;
-                }
-                write!(f, "{}  {}", self.status, self.title)?;
-            }
-        }
+        write!(f, "{}", self.title)?;
         if let Some(link) = &self.link {
             write!(f, " {}", format_hyperlink(link, "[link]").dimmed())?;
         }
@@ -70,15 +56,37 @@ impl fmt::Display for TaskItemWithIndex<'_> {
         let task = self.0;
         let index = self.1;
         let format = self.2;
+        write!(f, "{:>2}.{}", index, TaskItemWithStatus(task, format))?;
+        Ok(())
+    }
+}
+
+pub struct TaskItemWithStatus<'a>(pub &'a TaskItem, pub LinkFormat);
+
+impl fmt::Display for TaskItemWithStatus<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let task = self.0;
+        let format = self.1;
         let text = match format {
             LinkFormat::Adjacent => TaskItemCopyable(task).to_string(),
-            // TODO: these three types differ not only because of the link format; Hyperlink here
-            // also shows the status and star whereas the others do not. We are conflating link
-            // format with task format and they should be separate.
             LinkFormat::Hyperlink => task.to_string(),
             LinkFormat::Markdown => TaskItemCopyableMarkdown(task).to_string(),
         };
-        write!(f, "{:>2}.{}", index, text)?;
+        match task.status {
+            // Archived status doesn't require any extra formatting because it will never be mixed
+            // with other statuses and will never be starred.
+            Status::Archived => {
+                write!(f, "{}", task.title)?;
+            }
+            _ => {
+                if task.star.is_some_and(|v| v) {
+                    write!(f, "{} ", "★".yellow())?;
+                } else {
+                    write!(f, "  ")?;
+                }
+                write!(f, "{}  {}", task.status, text)?;
+            }
+        }
         Ok(())
     }
 }
